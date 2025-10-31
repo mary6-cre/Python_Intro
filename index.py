6973527e6258a97bdeb6a718abee097d69d0be60
Mary Watiri Kuria <marykuria680@gmail.com>
2025-10-31 23:24:15 +0300

    Initial commit: added full project files

---
+++ index.py
@@ -0,0 +1,13 @@
+Marks =int(input( "enter your score "))
+if Marks >= 90:
+    print("Grade A")
+elif Marks >= 80:
+    print("Grade B")
+elif Marks >= 70:
+    print("Grade C")
+elif Marks >= 60:
+    print("Grade D")
+elif Marks >= 50:
+    print("Grade E")
+else :
+    print("Grade F")
