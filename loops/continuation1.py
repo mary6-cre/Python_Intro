53fa0718f2cbb2cd89982c280bfa8a223b59538a
Mary Watiri Kuria <marykuria680@gmail.com>
2025-12-17 06:34:39 -0800

    your message

---
+++ loops/continuation1.py
@@ -0,0 +1,12 @@
+while True:
+    n=int(input("What's n?"))
+    if n>0:
+        break
+      # if n<0:
+       # continue # this will tell the loop to continue when the value of n is less than 0
+    # else :
+     #   break
+for _ in range (n):
+    print("meow")
+
+main()
---
+++ loops/continuation3.py
@@ -0,0 +1,10 @@
+def main():
+    meow(3)
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+main()
+
+
+
